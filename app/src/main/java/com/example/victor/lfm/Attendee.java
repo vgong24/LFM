245fb1c62bc79029e2d438f4bf8bddacca321edb
Victor Gong <vgong24@gmail.com>
2015-02-14 01:30:31 -1000

    Added Garry's classes
    gather data

---
+++ app/src/main/java/com/example/victor/lfm/Attendee.java
@@ -0,0 +1,33 @@
+package com.example.victor.lfm;
+
+/**
+ * Created by Victor on 2/14/2015.
+ */
+
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+public class Attendee extends ParseObject {
+    public void setAttend(boolean here) {
+        put("attend", here);
+    }
+    public boolean getAttend() {
+        return getBoolean("here");
+    }
+    public String getUser() {
+        return getString("userid");
+    }
+    public void setUser(String id) {
+        put("userid", id);
+    }
+    public String getEventID() {
+        return getString("event");
+    }
+    public void setEventID(String id) {
+        put("event", id);
+    }
+    public static ParseQuery<Attendee> getQuery() {
+        return ParseQuery.getQuery(Attendee.class);
+    }
+
+}
