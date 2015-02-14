24e958d6012acc7aa475cc21651e7a9a264f9315
Alan <alangyho@gmail.com>
2015-02-14 00:51:57 -1000

    Minor UI changes

--- app/src/main/java/com/example/victor/lfm/MainActivity.java
+++ app/src/main/java/com/example/victor/lfm/MainActivity.java
@@ -1,21 +1,59 @@
 package com.example.victor.lfm;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import com.parse.Parse;
+import com.parse.ParseUser;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends ActionBarActivity {
 
     Button logOut = null;
+    private static final int TIME_DIALOG_ID = 0 ;
+    TextView timeView;
+    Button timeBtn;
+    private TextView timeText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initTabs();
+
+        timeView = (TextView) findViewById(R.id.timeView);
+        timeBtn = (Button) findViewById(R.id.timeBtn);
+        timeBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new Mytimepicker(timeView);
+                newFragment.show(getFragmentManager(), "timePicker");
+            }
+        });
 
         logOut = (Button) findViewById(R.id.logout_btn);
+        logOut.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                startActivity(new Intent(v.getContext(), DispatchActivity.class));
+            }
+        });
 
 
     }
@@ -41,5 +79,63 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initTabs(){
+        TabHost tabhost = (TabHost) findViewById(R.id.tabHost);
+        tabhost.setup();
+        TabHost.TabSpec tabSpec = tabhost.newTabSpec("home");
+        tabSpec.setContent(R.id.homeTab);
+        tabSpec.setIndicator("Home");
+        tabhost.addTab(tabSpec);
+        tabSpec = tabhost.newTabSpec("search");
+        tabSpec.setContent(R.id.searchTab);
+        tabSpec.setIndicator("Search");
+        tabhost.addTab(tabSpec);
+        tabSpec = tabhost.newTabSpec("create");
+        tabSpec.setContent(R.id.createTab);
+        tabSpec.setIndicator("Create");
+        tabhost.addTab(tabSpec);
+        tabSpec = tabhost.newTabSpec("profile");
+        tabSpec.setContent(R.id.profileTab);
+        tabSpec.setIndicator("Profile");
+        tabhost.addTab(tabSpec);
+    }
+
+
+    public static class Mytimepicker extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+        TextView timeTxt;
+
+        public Mytimepicker(TextView txtview){
+            timeTxt = txtview;
+        }
+
+        public Mytimepicker() {
+
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+            TimePickerDialog timeDialog = new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+            timeDialog.setTitle("Test");
+            return timeDialog;
+        }
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+// do something with the time chosen. http://stackoverflow.com/questions/2659954/timepickerdialog-and-am-or-pm/2660148#2660148
+            String am_pm = "";
+            Calendar datetime = Calendar.getInstance();
+            datetime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            datetime.set(Calendar.MINUTE, minute);
+            if (datetime.get(Calendar.AM_PM) == Calendar.AM)
+                am_pm = "AM";
+            else if (datetime.get(Calendar.AM_PM) == Calendar.PM)
+                am_pm = "PM";
+            String strHrsToShow = (datetime.get(Calendar.HOUR) == 0) ?"12":datetime.get(Calendar.HOUR)+"";
+            timeTxt.setText(strHrsToShow + ":" + datetime.get(Calendar.MINUTE)+" " + am_pm);
+        }
     }
 }
