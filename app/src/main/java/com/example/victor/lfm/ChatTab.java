06c3efc79a56b5c23504164c60743f34d467b1a1
Victor Gong <vgong24@gmail.com>
2015-05-17 13:47:06 -1000

    ChatTab layout fixes

--- app/src/main/java/com/example/victor/lfm/ChatTab.java
+++ app/src/main/java/com/example/victor/lfm/ChatTab.java
@@ -1,31 +1,100 @@
 package com.example.victor.lfm;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
 import android.support.v4.app.Fragment;import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Victor on 5/15/2015.
  */
 public class ChatTab extends Fragment{
     Context context;
+    Activity activity;
+    private String currentUserId;
+    private ArrayAdapter<String> namesArrayAdapter;
+    private ArrayList<String> names;
+    private ListView usersListView;
+    private ProgressDialog progressDialog;
+    private BroadcastReceiver receiver = null;
 
     public ChatTab(Context context){
         this.context = context;
+        this.activity = (Activity) context;
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.tab_2,container,false);
-        TextView tv = (TextView)v.findViewById(R.id.textView);
-        tv.setText("It worked!");
+        View v = inflater.inflate(R.layout.chat_room_list,container,false);
+        initialize();
+        setConversationList();
         return v;
     }
+
+    private void initialize(){
+        currentUserId = ParseUser.getCurrentUser().getObjectId();
+        names = new ArrayList<String>();
+    }
+
+    //display clickable list of all users
+    private void setConversationList(){
+        names.clear();
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereNotEqualTo("objectId", currentUserId);
+        query.findInBackground(new FindCallback<ParseUser>(){
+
+            @Override
+            public void done(List<ParseUser> userList, ParseException e) {
+                if(e == null){
+                    for(int i = 0; i < userList.size(); i++){
+                        names.add(userList.get(i).getUsername().toString());
+                    }
+                    usersListView = (ListView) activity.findViewById(R.id.usersListView);
+                    namesArrayAdapter = new ArrayAdapter<String>(context, R.layout.chat_list_item, names);
+                    usersListView.setAdapter(namesArrayAdapter);
+
+                    usersListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            openConversation(names, position);
+                        }
+                    });
+
+
+                }else{
+                    Toast.makeText(context, "Error loading user list", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void openConversation(ArrayList<String> names, int pos){
+
+    }
+
+
+
 
 }
