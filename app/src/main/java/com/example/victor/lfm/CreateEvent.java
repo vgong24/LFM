1f194f3d5c82154a2e311e26104c1658f399cb4f
Victor Gong <vgong24@gmail.com>
2015-06-13 16:23:22 -1000

    Added static marker
    In create event xml, used a high resolution image to display a none
    moving marker at the center of the layout. To perfectly center the point
    to the center, double the height of the image.

--- app/src/main/java/com/example/victor/lfm/CreateEvent.java
+++ app/src/main/java/com/example/victor/lfm/CreateEvent.java
@@ -3,10 +3,21 @@ package com.example.victor.lfm;
 import android.app.Activity;
 import android.app.FragmentManager;
 
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Victor on 6/10/2015.
@@ -14,21 +25,43 @@ import android.support.v4.app.Fragment;
 public class CreateEvent extends FragmentActivity {
 
     CreateTab createTab;
+    FragmentTransaction transaction;
+    Fragment fragment;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
 
-        Fragment fragment = new CreateTab(CreateEvent.this);
-
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.createFrame, fragment);
-        transaction.commit();
-
+        fragment = new CreateTab(CreateEvent.this);
+        progressBar = (ProgressBar) findViewById(R.id.chatRoomProgressBar);
+        progressBar.setVisibility(View.VISIBLE);
+        transaction = getSupportFragmentManager().beginTransaction();
+        new SetUpBackground().execute();
 
 
     }
+
+    private class SetUpBackground extends AsyncTask<Void, Void, Void> {
+        ArrayList<Attendee> attendeesArr;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            transaction.replace(R.id.createFrame, fragment);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void none){
+            progressBar.setVisibility(View.GONE);
+            transaction.commit();
+
+
+        }
+    }
+
 
 
 }
